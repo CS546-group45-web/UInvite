00ec6331bf99cc7e76373fae4271118a024ce5c0
manisaiprasad <manisaiprasadam@gmail.com>
2022-12-15 01:11:22 -0500

    clean up PR

---
+++ server/utils/passportStrategy.js
@@ -0,0 +1,22 @@
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+const users = require('../data/users');
+
+const opts = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+const strategy = new JWTStrategy(opts, (payload, done) => {
+  users.getUserById(payload.id).then((user) => {
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+module.exports = strategy;
