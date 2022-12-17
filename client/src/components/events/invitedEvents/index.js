f78ccc2eef05f362e7351eb83697bd0e3379c7cf
tarun8698 <tdadlani@stevens.edu>
2022-12-17 14:18:12 -0500

    Added: - Invites list page

--- client/src/components/events/invitedEvents/index.js
+++ client/src/components/events/invitedEvents/index.js
@@ -1,192 +1,55 @@
 import React from "react";
+import Loading from "../../common/Loading";
+import EventCard from "../lists/eventCard";
+import { getUserDetails, getUserInvites } from "../../../utils/apis/user";
+import { toast } from "react-toastify";
+import EmptyBox from "../../../assets/images/emptybox.png";
+import "./styles.css";
 
-function EventsListInvited() {
+function InvitedEvents() {
+  const [events, setEvents] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [loggedInUserData, setLoggedInUserData] = React.useState({});
+
+  React.useEffect(() => {
+    setLoading(true);
+    getUserInvites().then((res) => {
+      const { data, status } = res;
+      if (status !== 200) {
+        setLoading(false);
+        return toast.error("Failed to fetch events");
+      } else setEvents(data?.data);
+      getUserDetails().then((res) => {
+        const { data, status } = res;
+        if (status !== 200) return toast.error(data.error);
+        setLoggedInUserData(data);
+        setLoading(false);
+      });
+    });
+  }, []);
   return (
     <div className="w-full">
-      <h2 className="mt-6 text-center text-5xl font-bold tracking-tight text-gray-900">
-        Your Invitations
-      </h2>
-      <header className="mt-6 text-left text-2xl  tracking-tight text-gray-900">Search through Events that you have been invited to here!</header>
-      
-      
-      {/* <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer>
-      <header>Header</header>
-      This page will have sample details of events listing and details about the
-      page like title of the page, header, body, footer
-      <footer>Footer</footer> */}
+      <div className="text-5xl font-semibold mt-4 mb-8">Invitation/s</div>
+      {loading ? (
+        <div className="h-full w-full flex items-center justify-center">
+          <Loading loading={loading} width={40} color="#393e46" thickness={5} />
+        </div>
+      ) : events?.length !== 0 ? (
+        events?.map((event) => (
+          <EventCard
+            event={event}
+            userId={loggedInUserData?._id}
+            key={event?._id}
+          />
+        ))
+      ) : (
+        <div className="empty_box_text">
+          <img src={EmptyBox} alt="no invites found" width={200} />
+          <span className="text-4xl font-light">No Events Found.</span>
+        </div>
+      )}
     </div>
   );
 }
 
-export default EventsListInvited;
+export default InvitedEvents;
