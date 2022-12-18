7795165f758c5ee38b4ccbe3fbbb6b3c103b99db
tarun8698 <tdadlani@stevens.edu>
2022-12-18 01:22:57 -0500

    bookmark section

--- client/src/components/profile/bookmarks.js
+++ client/src/components/profile/bookmarks.js
@@ -1,15 +1,20 @@
 import React from "react";
-import EventCard from "../events/lists/eventCardHome";
+import EventCardBookmark from "../events/lists/eventCardBookmark";
+import BookmarkAddedOutlinedIcon from "@mui/icons-material/BookmarkAddedOutlined";
 
 function Bookmarks({ events }) {
   return (
-    <div>
-      <div>Bookmarks</div>
-      {events?.length !== 0
-        ? events?.map((event) => (
-            <EventCard event={event} userId={null} key={event?._id} />
-          ))
-        : null}
+    <div className="m-4">
+      <div className="text-2xl font-bold border-b-2 text-[#393e46]">
+        <BookmarkAddedOutlinedIcon /> Your Bookmarks
+      </div>
+      <div className="events_grid_profile">
+        {events?.length !== 0
+          ? events?.map((event) => (
+              <EventCardBookmark event={event} key={event?._id} />
+            ))
+          : null}
+      </div>
     </div>
   );
 }
