1efaa9b42f20cff5f65e8d2f7872f243574ab4a2
manisaiprasad <manisaiprasadam@gmail.com>
2022-12-18 16:32:21 -0500

    added deleteCalendarEvent

--- server/calendar/calendarEvents.js
+++ server/calendar/calendarEvents.js
@@ -78,8 +78,35 @@ const updateCalendarEvent = async (calendarEventId, userData, event) => {
   }
 };
 
+const deleteCalendarEvent = async (calendarEventId, userData) => {
+  // get user googleCalendarDetails
+  try {
+    const { googleCalendarDetails } = userData;
+    // set credentials
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET
+    );
+
+    oauth2Client.setCredentials({
+      access_token: googleCalendarDetails.googleAccessToken,
+      refresh_token: googleCalendarDetails.googleRefreshToken,
+    });
+    const calendar = google.calendar({ version: 'v3', oauth2Client });
+    const res = await calendar.events.delete({
+      auth: oauth2Client,
+      calendarId: 'primary',
+      eventId: calendarEventId,
+    });
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+};
+
 module.exports = {
   getCalendarUpcomingEvents,
   createCalendarEvent,
   updateCalendarEvent,
+  deleteCalendarEvent,
 };
