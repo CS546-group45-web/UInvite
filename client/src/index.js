a0602f268134d3730c70a13232c467c29456a47c
manisaiprasad <manisaiprasadam@gmail.com>
2022-12-18 16:54:31 -0500

    removed GoogleOAuthProvider from client

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-    <App />
-  </GoogleOAuthProvider>
+  <App />
 
   // </React.StrictMode>
 );
