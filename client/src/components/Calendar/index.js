be1360e27520eed7985353e44e3b6bcec4d37f9f
manisaiprasad <manisaiprasadam@gmail.com>
2022-12-18 21:38:56 -0500

    added CalendarHelper for Google calander

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,52 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
-import { createAuthLink } from '../../utils/apis/auth';
 import { getUserDetails } from '../../utils/apis/user';
-
+import { createAuthLink } from '../../utils/apis/auth';
+import CalendarHelper from './CalendarHelper';
 function Calendar() {
   const [user, setUser] = useState(null);
-  const setting = {
-    googleCalendarApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-    eventSources: [
-      {
-        googleCalendarId: 'manisaiprasadam@gmail.com',
-      },
-    ],
-    initialView: 'dayGridMonth',
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay',
-    },
-    themeSystem: 'Simplex',
-    plugins: [
-      googleCalendarPlugin,
-      dayGridPlugin,
-      timeGridPlugin,
-      interactionPlugin,
-    ],
-    visibleRange: {
-      start: '2020-03-22',
-      end: '2020-03-25',
-    },
-    navLinks: true,
-    weekNumbers: true,
-    weekText: '',
-    selectable: true,
-    nowIndicator: true,
-    select: (info) => {
-      console.log(info);
-    },
-    eventClick: (info) => {
-      // open event in new tab
-      info.jsEvent.preventDefault();
-      window.open(info.event.url, 'gcalevent', 'width=700,height=600').focus();
-    },
-  };
 
   const getUser = async () => {
     const { data } = await getUserDetails();
@@ -59,14 +16,7 @@ function Calendar() {
   }, []);
 
   const getUserAccesdetails = async () => {
-    if (user?.googleConnected) {
-      const { googleCalendarId } = user;
-      setting.eventSources = [
-        {
-          googleCalendarId,
-        },
-      ];
-    } else {
+    if (!user?.googleConnected) {
       createAuthLink().then((res) => {
         const {
           data: { url },
@@ -79,7 +29,9 @@ function Calendar() {
   return (
     <div className="p-4">
       {user?.googleConnected ? (
-        <FullCalendar {...setting} />
+        <CalendarHelper
+          googleCalendarId={user?.googleCalendarDetails.googleCalendarId}
+        />
       ) : (
         <button className="btn_default" onClick={getUserAccesdetails}>
           Connect to google calendar
