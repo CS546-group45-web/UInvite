a17c3b435bea73b7f4350050c83043c2fdc5b507
tarun8698 <tdadlani@stevens.edu>
2022-12-18 19:58:57 -0500

    Added google button to calendar page

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
+import { createAuthLink } from "../../utils/apis/auth";
+import { getUserDetails } from "../../utils/apis/user";
 
 function Calendar() {
   const setting = {
     googleCalendarApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     eventSources: [
       {
-        googleCalendarId: 'manisaiprasadam@gmail.com',
+        googleCalendarId: "manisaiprasadam@gmail.com",
       },
     ],
-    initialView: 'dayGridMonth',
+    initialView: "dayGridMonth",
     headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay',
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth,timeGridWeek,timeGridDay",
     },
-    themeSystem: 'Simplex',
+    themeSystem: "Simplex",
     plugins: [
       googleCalendarPlugin,
       dayGridPlugin,
@@ -27,12 +29,12 @@ function Calendar() {
       interactionPlugin,
     ],
     visibleRange: {
-      start: '2020-03-22',
-      end: '2020-03-25',
+      start: "2020-03-22",
+      end: "2020-03-25",
     },
     navLinks: true,
     weekNumbers: true,
-    weekText: '',
+    weekText: "",
     selectable: true,
     nowIndicator: true,
     select: (info) => {
@@ -41,11 +43,30 @@ function Calendar() {
     eventClick: (info) => {
       // open event in new tab
       info.jsEvent.preventDefault();
-      window.open(info.event.url, 'gcalevent', 'width=700,height=600').focus();
+      window.open(info.event.url, "gcalevent", "width=700,height=600").focus();
     },
   };
+
+  const getUserAccesdetails = async () => {
+    getUserDetails().then((res) => {
+      const { data } = res;
+      if (!data?.googleConnected || data?.googleConnected === false)
+        createAuthLink().then((res) => {
+          const {
+            data: { url },
+          } = res;
+          debugger;
+          window.location.href = url;
+        });
+      else window.location.href = "/";
+    });
+  };
+
   return (
     <div className="p-4">
+      <button className="btn_default" onClick={getUserAccesdetails}>
+        Connect to google calendar
+      </button>
       <FullCalendar {...setting} />
     </div>
   );
